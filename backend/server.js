8f7be704dd27ea89282c6d87286c094741774d72
AnupNair08 <nair.anup09@gmail.com>
2020-10-11 14:56:03 +0530

    chore: setup frontend

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const mysql = require('mysql')
+const port = process.env.PORT || 5000
+const app = express();
+require('dotenv').config()
+
+const connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : process.env.DBpass,
+    database : 'paytm'
+})
+connection.connect(e => {
+    if(e){
+        console.log(e)
+    }
+    else{
+        console.log('Connected to MySQL Client')
+    }
+})
+
+app.listen(port, () => {
+    console.log(process.env.DBpass)
+    console.log(`Listening to ${port}`)
+})
+
+app.get('/',()=>{
+    connection.query('SELECT * FROM customer', (e,res) =>{
+        if(e){
+            console.log(e)
+        }
+        else{
+            console.log(res)
+        }
+    })
+})
