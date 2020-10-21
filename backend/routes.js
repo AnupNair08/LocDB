47b36a43e5b7f2f02f62134111357cab576fc52a
AnupNair08 <nair.anup09@gmail.com>
2020-10-21 18:35:55 +0530

    feat: ui improvements

--- backend/routes.js
+++ backend/routes.js
@@ -15,4 +15,30 @@ router.post('/mylocation', async (req,res) => {
     })
 })
 
+router.post('/update', async(req,res) => {
+    const {driver_id, zipcode} = req.body
+    connection.query(`UPDATE present_at set zipcode="${zipcode}" where driver_id="${driver_id}"`, (e,op) => {
+        if(e){
+            console.log(e)
+            return res.status(404).json({'msg' : 'Error'})
+        }
+        return res.status(200).json({'msg' : 'Success'})
+
+    })
+})
+
+router.post('/getshift', async(req,res) => {
+    const {driver_id} = req.body
+    connection.query(`SELECT * FROM shifts where shift_id in (SELECT shift_id from works where driver_id="${driver_id}")`, (e,op) => {
+        if(e){
+            console.log(e)
+            return res.status(404).json({'msg' : 'Error'})
+        }
+        else{
+            return res.status(200).json({'msg' : 'Success', shifts : op})
+        }
+    })
+})
+
+
 module.exports = router
--- src/pages/driv.js
+++ src/pages/driv.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import Header from './header'
-import {ListGroup, ListGroupItem, Button} from 'reactstrap'
+import {ListGroup, ListGroupItem, Button, Dropdown, Card, CardTitle, CardText} from 'reactstrap'
 import axios from 'axios'
+import Location from './loc'
+import ReactNotification, { store } from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css'
+
 export default class DriverPage extends Component {
     componentDidMount = async () => {
         await axios({
@@ -18,17 +22,20 @@ export default class DriverPage extends Component {
         }).catch(e => {
             console.log(e)
         })
+    
         await axios({
             method : 'post',
-            url : 'http://localhost:5000/getlocation',
+            url : 'http://localhost:5000/api/getshift',
+            data : {
+                driver_id : this.props.data.driver_id
+            }
         }).then((res) => {
             console.log(res)
             this.setState({
-                location : res.data
+                shifts : res.data.shifts
             })
-        }).catch(e => {
-            console.log(e)
         })
+        
     }
 
     getloc = () => {
@@ -51,29 +58,41 @@ export default class DriverPage extends Component {
         return (
             <div>
                 <Header></Header>
-                <div style= {{display : 'flex' , flexDirection : 'row', justifyContent : 'center', alignItems : 'center'}}>
-                <img src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png" height="200px" width = "200px"></img>
-        <h3>Hello {this.props.data.d_name}</h3>
+                <ReactNotification />
+                <div style = {{display : 'flex', flexDirection : 'row', justifyContent : 'center', alignItems : 'center'}}>
+
+                <div style= {{display : 'flex' , flexDirection : 'column', justifyContent : 'center', alignItems : 'center'}}>
+                <img src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png" height="200px" width = "200px"></img><br></br>
+                    <h3>Hello {this.props.data.d_name}</h3>
                 </div>
-                <h3>
-                    My Profile
-                </h3>
-                <ListGroup>
-                <ListGroupItem>Phone Number: {this.props.data.d_phone_no}</ListGroupItem>
-                {/* {this.state && console.log(this.state.taxi.data[0].model)} */}
-                {
-                    this.state && 
-                    <ListGroupItem>My Taxi: {this.state.taxi.data[0].model}[{this.state.taxi.data[0].color}] 
-                    {this.state.taxi.data[0].number}</ListGroupItem>
-                }
-                <ListGroupItem>Rating : {this.props.data.rating}</ListGroupItem>
-                </ListGroup>
+                <Card body inverse>
+                    <CardTitle className ="text-dark"><h2>My Profile</h2></CardTitle>
+                    <ListGroup>
+                    <ListGroupItem className="text-dark">Phone Number: {this.props.data.d_phone_no}</ListGroupItem>
+                    {
+                        this.state && 
+                        <ListGroupItem className="text-dark">My Taxi: {this.state.taxi.data[0].model}[{this.state.taxi.data[0].color}] 
+                        {this.state.taxi.data[0].number}</ListGroupItem>
+                    }
+                    <ListGroupItem className="text-dark">Rating : {this.props.data.rating}<img src="https://i.pinimg.com/736x/68/9f/52/689f5245bf682fc77d8346f69e082fba.jpg"
+                    height = "20px" width = "20px"></img></ListGroupItem>
+                    </ListGroup>
+                    
+              </Card>
+
+                </div>
                 <h3>My Location:</h3>
                 <Button onClick = {this.getloc}>Get Current Location</Button>
                 {
-                    this.state && this.state.myloc ? <h3>{this.state.myloc}</h3> : <h3></h3>
+                    this.state && this.state.myloc ? 
+                        <h3 className="mt-1"><img src = "https://i.pinimg.com/originals/29/93/fd/2993fd151e2e1cab871aec155e22cbcc.png" height="40px" width="30px"></img>   {this.state.myloc}</h3>
+                    : <h3></h3>
+
                 }
-            </div>
+                <Location driver = {this.props.data.driver_id}/>    
+                <h3>My Shift</h3>     
+            {this.state && this.state.shifts && <h3>{this.state.shifts[0].start}  {this.state.shifts[0].end}</h3>}       
+                </div>
         )
     }
 }
