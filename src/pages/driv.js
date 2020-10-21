828ccb7eee2cb436efa7207c3b212ff192b06db7
AnupNair08 <nair.anup09@gmail.com>
2020-10-21 16:18:58 +0530

    feat: frontend for locations

--- src/pages/driv.js
+++ src/pages/driv.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import Header from './header'
-import {ListGroup, ListGroupItem} from 'reactstrap'
+import {ListGroup, ListGroupItem, Button} from 'reactstrap'
 import axios from 'axios'
 export default class DriverPage extends Component {
-    componentDidMount = () => {
-        axios({
+    componentDidMount = async () => {
+        await axios({
             method : 'post',
             url : 'http://localhost:5000/gettaxi',
             data : {
@@ -18,6 +18,34 @@ export default class DriverPage extends Component {
         }).catch(e => {
             console.log(e)
         })
+        await axios({
+            method : 'post',
+            url : 'http://localhost:5000/getlocation',
+        }).then((res) => {
+            console.log(res)
+            this.setState({
+                location : res.data
+            })
+        }).catch(e => {
+            console.log(e)
+        })
+    }
+
+    getloc = () => {
+        axios({
+            method : 'post',
+            url : 'http://localhost:5000/api/mylocation',
+            data : {
+                driver_id : this.props.data.driver_id
+            }
+        }).then((res) => {
+            console.log(res)
+            this.setState({
+                myloc : res.data.data[0].loc_name
+            })
+        }).catch(e => {
+            console.log(e)
+        })
     }
     render() {
         return (
@@ -25,7 +53,7 @@ export default class DriverPage extends Component {
                 <Header></Header>
                 <div style= {{display : 'flex' , flexDirection : 'row', justifyContent : 'center', alignItems : 'center'}}>
                 <img src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png" height="200px" width = "200px"></img>
-                <h3>Hello [Driver]</h3>
+        <h3>Hello {this.props.data.d_name}</h3>
                 </div>
                 <h3>
                     My Profile
@@ -40,6 +68,11 @@ export default class DriverPage extends Component {
                 }
                 <ListGroupItem>Rating : {this.props.data.rating}</ListGroupItem>
                 </ListGroup>
+                <h3>My Location:</h3>
+                <Button onClick = {this.getloc}>Get Current Location</Button>
+                {
+                    this.state && this.state.myloc ? <h3>{this.state.myloc}</h3> : <h3></h3>
+                }
             </div>
         )
     }
